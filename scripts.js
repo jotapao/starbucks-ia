623f43ac059e421ca3d647e96b63891b2070d136
jotapao <joaopedrocordeiro12.jops@gmail.com>
2025-11-12 17:34:37 -0300

    adicionando funcionalidade de troca de imagem junto a troca da cor

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-let circulo = document.querySelector(".circulo")
+// Seleciona os elementos do HTML
+let circulo = document.querySelector(".circulo");
+let imagemCopo = document.querySelector(".imagem-copo"); 
 
-function mudarCor(cor){
-
-    circulo.style.backgroundColor = cor
-}
-
-function trocaImagem(imagem){
-    imagemCopo.src = imagem
+// Criamos uma ÚNICA função que recebe DOIS parâmetros
+function mudarProduto(cor, imagem) {
+    circulo.style.backgroundColor = cor; // Altera a cor do círculo
+    imagemCopo.src = imagem;              // Altera a imagem do copo
 }
